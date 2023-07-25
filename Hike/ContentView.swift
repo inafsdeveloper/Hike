3a93537b9614993abf4846e1891de5cf411b0a85
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-25 12:25:31 -0700

    Initial Setup

---
+++ Hike/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Pankaj Nafria on 7/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Pankaj Nafria on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
